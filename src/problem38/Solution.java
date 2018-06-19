fd6f82e79f8b94ba8c122f76c717e2d2ccaf5550
Chuan He <chuanheee@gmail.com>
2018-06-18 22:51:17 -0700

    Problem 238,66,38

---
+++ src/problem38/Solution.java
@@ -0,0 +1,29 @@
+package problem38;
+
+class Solution {
+    public String countAndSay(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(1);
+        for (int i=1; i<n; i++) {
+            char lastNum = sb.charAt(0);
+            int count = 0;
+            StringBuilder newSb = new StringBuilder();
+            for (int j=0; j < sb.length(); j++) {
+                if (sb.charAt(j) != lastNum) {
+                    newSb.append(count);
+                    newSb.append(lastNum);
+                    count = 0;
+                    lastNum = sb.charAt(j);
+                }
+                count++;
+            }
+            newSb.append(count);
+            newSb.append(lastNum);
+            sb = newSb;
+        }
+
+        return sb.toString();
+    }
+
+
+}
