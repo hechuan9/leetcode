3e7eeeb6bf5afc900fe08eb7e2c8694883e6566b
Chuan He <chuanheee@gmail.com>
2018-07-26 23:29:52 -0700

    Problem 206, 237, 206

---
+++ src/problem206/Solution.java
@@ -0,0 +1,17 @@
+package problem206;
+
+import support.ListNode;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode node = head;
+        ListNode reverse = null;
+        while (node != null) {
+            ListNode nextNode = node.next;
+            node.next = reverse;
+            reverse = node;
+            node = nextNode;
+        }
+        return reverse;
+    }
+}
