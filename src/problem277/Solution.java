a1a4d995206217667a5601e4e6bbf9d61e41e157
Chuan He <chuanheee@gmail.com>
2018-07-31 21:42:44 -0700

    Problem 227,277,94

--- src/problem277/Solution.java
+++ src/problem277/Solution.java
@@ -1,11 +1,12 @@
 package problem277;
 
 
-import javax.management.relation.Relation;
-import java.util.LinkedList;
-import java.util.List;
+public class Solution {
+    // Fake code
+    private boolean knows(int a, int b) {
+        return true;
+    }
 
-public class Solution extends Relation {
     public int findCelebrity(int n) {
         int celebrity = 0;
         for (int i = 1; i < n; i++) {
---
+++ src/problem94/RecursiveSolution.java
@@ -0,0 +1,31 @@
+package problem94;
+
+import support.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class RecursiveSolution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ret = new LinkedList<>();
+        inorder(ret, root);
+        return ret;
+    }
+
+    private void inorder(List<Integer> list, TreeNode node) {
+        if (node == null) return;
+
+        inorder(list, node.left);
+        list.add(node.val);
+        inorder(list, node.right);
+    }
+}
