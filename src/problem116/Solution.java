c60b9f3aad09ee26746167bfbdc77530e9b55c32
Chuan He <chuanheee@gmail.com>
2018-08-04 16:19:55 -0700

    Problem 103,116,253,279

---
+++ src/problem116/Solution.java
@@ -0,0 +1,23 @@
+package problem116;
+
+import support.TreeLinkNode;
+
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if (root == null) return;
+        travel(root.left, root.right);
+        travel(root.right, null);
+    }
+
+    private void travel(TreeLinkNode node, TreeLinkNode next) {
+        if (node == null) return;
+
+        travel(node.left, node.right);
+
+        if (next == null) travel(node.right, null);
+        else travel(node.right, next.left);
+
+
+        node.next = next;
+    }
+}
