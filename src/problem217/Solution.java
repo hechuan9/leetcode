089015535c04335c50ccb7eb6ac6e8a836bcef53
Chuan He <chuanheee@gmail.com>
2018-08-04 11:54:14 -0700

    Problem 136,202,217,387

---
+++ src/problem217/Solution.java
@@ -0,0 +1,17 @@
+package problem217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (set.contains(i)) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
