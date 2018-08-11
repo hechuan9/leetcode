7b37881d94cbb33239f6fa3f5465f40aeafdffee
Chuan He <chuanheee@gmail.com>
2018-08-10 20:41:00 -0700

    Problem 127,2,49,55,98

---
+++ src/problem49/Solution.java
@@ -0,0 +1,31 @@
+package problem49;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c-'a']++;
+            }
+            StringBuilder hashBuilder = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if (count[i] != 0) {
+                    hashBuilder.append(count[i]);
+                    hashBuilder.append('a'+i);
+                }
+            }
+            String hash = hashBuilder.toString();
+            List<String> value = map.getOrDefault(hash, new LinkedList<>());
+            value.add(s);
+            map.put(hash, value);
+        }
+        return map.values().stream().collect(Collectors.toList());
+    }
+}
