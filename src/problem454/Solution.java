3fb3c3fa5d944ef230798971d3dfa53a93f146b0
Chuan He <chuanheee@gmail.com>
2018-08-14 22:26:43 -0700

    Problem 46,454

---
+++ src/problem454/Solution.java
@@ -0,0 +1,25 @@
+package problem454;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int fourSumCount(int[] a, int[] b, int[] c, int[] d) {
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                int sum = a[i] + b[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < d.length; j++) {
+                int sum = -(c[i] + d[j]);
+                res += map.getOrDefault(sum, 0);
+            }
+        }
+        return res;
+    }
+}
