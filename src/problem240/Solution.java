fe858fa5c7ac72c304585f82524eb2730da5d2b8
Chuan He <chuanheee@gmail.com>
2018-08-18 20:41:01 -0700

    Problem 162,240,704,75

---
+++ src/problem240/Solution.java
@@ -0,0 +1,16 @@
+package problem240;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (target == matrix[i][j]) return true;
+            else if (target > matrix[i][j]) i++;
+            else j--;
+        }
+        return false;
+    }
+}
