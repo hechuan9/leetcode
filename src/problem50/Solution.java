933a40fb52211f3fd4b8f293a47e18ea70e5e9ee
Chuan He <chuanheee@gmail.com>
2018-09-02 20:25:45 -0700

    Problem 324, 50

---
+++ src/problem50/Solution.java
@@ -0,0 +1,18 @@
+package problem50;
+
+class Solution {
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            x = 1/x;
+            n = -n;
+        }
+        double ret = 1;
+        double cur = x;
+        while (n != 0) {
+            if (n % 2 == 1) ret *= cur;
+            cur = cur * cur;
+            n /= 2;
+        }
+        return ret;
+    }
+}
